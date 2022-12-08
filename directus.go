a710d28564cdc5cbd1bcab44486abaa7a3bb2613
Jake Ceballos <jake@ceballos.tech>
2022-12-08 10:32:08 -0600

    remove logging output

--- directus.go
+++ directus.go
@@ -237,7 +237,7 @@ func (dc *Client) UpdateItem(item ICollectionItem) (any, error) {
 
 	// item.SetFieldsFromCollectionItem(body)
 
-	fmt.Println(string(body))
+	// fmt.Println(string(body))
 
 	err = json.Unmarshal(body, &item)
 
